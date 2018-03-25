a8ed5789a239f9988ed02365eaa3db93eea14d4e
Greg Strobl <gstrobl17@msn.com>
2018-03-25 10:38:33 -0400

    Added implementation with interactor as the interface implemented with view loaded from storyboard

---
+++ iOS/Module.xctemplate/InteractorStoryboard/___FILEBASENAME___Interactor.swift
@@ -0,0 +1,24 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+class ___VARIABLE_productName:identifier___Interactor {
+
+    weak var presenter: ___VARIABLE_productName:identifier___InteractorOutputProtocol?
+    weak var delegate: ___VARIABLE_productName:identifier___InterfaceDelegate?
+
+}
+
+extension ___VARIABLE_productName:identifier___Interactor: ___VARIABLE_productName:identifier___InteractorInputProtocol {
+
+}
+
+extension ___VARIABLE_productName:identifier___Interactor: ___VARIABLE_productName:identifier___InterfaceProtocol {
+
+}
---
+++ iOS/Module.xctemplate/InteractorStoryboard/___FILEBASENAME___Router.swift
@@ -0,0 +1,31 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+class ___VARIABLE_productName:identifier___Router: ___VARIABLE_productName:identifier___WireframeProtocol {
+
+    weak var viewController: UIViewController?
+
+    static func createModule() -> (view: UIViewController, interface: ___VARIABLE_productName:identifier___InterfaceProtocol) {
+        // swiftlint:disable force_cast todo
+        //TODO: set real storyboard name and view controller identifier
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let view = storyboard.instantiateViewController(withIdentifier: "___VARIABLE_productName:identifier___ViewController") as! ___VARIABLE_productName:identifier___ViewController
+        // swiftlint:enable force_cast todo
+        let interactor = ___VARIABLE_productName:identifier___Interactor()
+        let router = ___VARIABLE_productName:identifier___Router()
+        let presenter = ___VARIABLE_productName:identifier___Presenter(interface: view, interactor: interactor, router: router)
+
+        view.presenter = presenter
+        interactor.presenter = presenter
+        router.viewController = view
+
+        return (view, interactor)
+    }
+}
