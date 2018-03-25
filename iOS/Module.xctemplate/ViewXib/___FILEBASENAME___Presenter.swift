492c6db1889ce0e8edbce397f5e9757ddb9d950b
Greg Strobl <gstrobl17@msn.com>
2018-03-25 08:52:19 -0400

    Added implementation of module interaface in the view with xib

---
+++ iOS/Module.xctemplate/ViewXib/___FILEBASENAME___Presenter.swift
@@ -0,0 +1,31 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+class ___VARIABLE_productName:identifier___Presenter {
+
+    weak internal var view: ___VARIABLE_productName:identifier___ViewProtocol?
+    var interactor: ___VARIABLE_productName:identifier___InteractorInputProtocol?
+    let router: ___VARIABLE_productName:identifier___WireframeProtocol
+
+    init(interface: ___VARIABLE_productName:identifier___ViewProtocol, interactor: ___VARIABLE_productName:identifier___InteractorInputProtocol?, router: ___VARIABLE_productName:identifier___WireframeProtocol) {
+        self.view = interface
+        self.interactor = interactor
+        self.router = router
+    }
+
+}
+
+extension ___VARIABLE_productName:identifier___Presenter: ___VARIABLE_productName:identifier___PresenterProtocol {
+
+}
+
+extension ___VARIABLE_productName:identifier___Presenter: ___VARIABLE_productName:identifier___InteractorOutputProtocol {
+
+}
